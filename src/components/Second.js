a66671b35b6aba1b7ceccf9c65822a57981a5348
nejahneji <nejinejah6@gmail.com>
2021-06-14 11:13:14 +0100

    first react

---
+++ src/components/Second.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "react-bootstrap";
+const Second = () => {
+    return (
+        <div>
+             <Button variant="success">Success</Button>{' '}
+ 
+        </div>
+    )
+}
+
+export default Second
